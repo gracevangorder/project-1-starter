8aecc4dd3c584597cfd1013957e25f8e5da6232e
gracevangorder <gracevangorder@mines.edu>
2024-09-03 20:50:37 -0600

    Update reverse.c

--- reverse.c
+++ reverse.c
@@ -2,6 +2,9 @@
 
 int main(int argc, char *argv[])
 {
+	printf("Please enter the name of the file you would like to reverse.");
+	
+	char *filename="in
 	printf("TODO: write a main function\n");
 	return 0;
 }
